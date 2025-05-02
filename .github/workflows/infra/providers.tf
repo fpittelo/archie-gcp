0bb14f5f1e14f5de5687d86132a7aed839fdbf68
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2025-05-02 15:42:47 +0200

    Create Devops CI/CD set up #1

---
+++ .github/workflows/infra/providers.tf
@@ -0,0 +1,31 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 6.20.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = "~> 6.20.0"
+    }
+    http = {
+      source  = "terraform-aws-modules/http"
+      version = "~> 2.0"
+    }
+  }
+
+  backend "gcs" {
+    bucket = "iaac-gcp-data-mgt-terraform"
+    prefix = "backend/state"
+  }
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.location
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.location
+}
---
+++ .github/workflows/infra/variables.tf
@@ -0,0 +1,11 @@
+###### Project Variables ######
+
+variable "project_id" {
+    description = "The GCP project ID"
+    type        = string
+}
+
+variable "location" {
+  description = "Resource location"
+  type        = string
+}
