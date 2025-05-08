bb23762d7e67a25aa22b29ddca0c611a4b8bb263
frederic.pitteloud@gmail.com <frederic.pitteloud@gmail.com>
2025-05-08 15:23:11 +0200

    Create Devops CI/CD set up #1

--- infra/main.tf
+++ infra/main.tf
@@ -5,32 +5,3 @@ resource "google_storage_bucket" "archiemcp_bucket" {
   location                    = var.location
   uniform_bucket_level_access = true
 }
-
-data "archive_file" "default" {
-  type        = "zip"
-  output_path = "/tmp/function-source.zip"
-  source_dir  = "../function"
-}
-
-#### Create Storage bucked object ####
-resource "google_storage_bucket_object" "function" {
-  depends_on = [ google_storage_bucket.archiemcp_bucket ]
-  name   = "function-source.zip"
-  bucket = var.storage_bucket
-  source = data.archive_file.default.output_path
-}
-
-resource "google_cloudfunctions_function" "archiefunct" {
-  depends_on = [ google_storage_bucket.archiemcp_bucket ]
-  name        = var.cloudfunction
-  project     = var.project_id
-  description = "A function to process Pub/Sub events"
-  runtime     = "python311"
-
-}
-
-resource "google_project_iam_member" "cloud_function_invoker" {
-  project = var.project_id
-  role    = "roles/cloudfunctions.invoker"
-  member  = "allUsers"
-}
