f97d95b6964a2b13c2f0f1a1dec71347607117bb
frederic.pitteloud@epfl.ch <frederic.pitteloud@epfl.ch>
2025-05-16 15:05:48 +0200

    Remove output for source code bucket name from ArchieMCP function outputs @fpittelo  #8

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -8,8 +8,3 @@ output "archiemcp_function_service_account_email" {
   description = "Email of the service account used by the ArchieMCP function."
   value       = google_service_account.archiemcp_function_sa.email
 }
-
-output "archiemcp_function_source_code_bucket_name" {
-  description = "Name of the GCS bucket storing the ArchieMCP function's source code."
-  value       = google_storage_bucket.archiemcp_function_source_bucket.name
-}
